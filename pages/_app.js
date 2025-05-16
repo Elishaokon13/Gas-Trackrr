2b35fc9303e526c00c578b3c3cf917ee106f52b6
Elishaokon13 <eliscodavid@gmail.com>
2025-05-16 04:42:15 +0100

    modified pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <Head>
-        <title>Based Baby | Your Base Wrapped</title>
+        <title | Your Base Wrapped</title>
         <meta name="description" content="Check your transaction count and gas spent on Base blockchain" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Roboto+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
