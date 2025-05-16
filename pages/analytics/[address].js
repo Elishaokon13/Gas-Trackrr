a887a1c47caecb35078fa155fab3bd8667111480
Elishaokon13 <eliscodavid@gmail.com>
2025-05-16 03:06:21 +0100

    modified pages/analytics/[address].js

--- pages/analytics/[address].js
+++ pages/analytics/[address].js
@@ -131,7 +131,7 @@ export default function AnalyticsPage() {
   return (
     <div className="min-h-screen bg-black text-white relative overflow-hidden">
       <Head>
-        <title>Gas  | {displayAddress}</title>
+        <title>Gas Tracker | {displayAddress}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       
