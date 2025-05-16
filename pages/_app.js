98852478a903001701d06fe12e34315cbc4c0f62
Elishaokon13 <eliscodavid@gmail.com>
2025-05-16 02:46:47 +0100

    modified pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,10 @@ function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <Head>
-        <title>Base Wrapped | Your Year on Base</title>
-        <meta name="description" content="Your personalized Base blockchain year in review" />
+        <title>Based Baby | Your Base Wrapped</title>
+        <meta name="description" content="Check your transaction count and gas spent on Base blockchain" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Roboto+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
       </Head>
       <AnimatePresence mode="wait">
         <Component {...pageProps} key={router.route} />
